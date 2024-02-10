fc076c968b6108dffbd29bba041390d691cfc96c
Roberto Junior <robertojuniorazul@gmail.com>
2024-02-10 04:43:14 -0300

    finalizado o layout da home

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import Logo from './assets/nlw-expert-logo.svg'
+import { NewNodeCard } from './components/new-note-card'
+import { NodeCard } from './components/note-card'
+
 export function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+      <img src={Logo}/>
+
+      <form className='w-full '>
+        <input 
+          type="text" 
+          placeholder='Busque em suas notas' 
+          className='w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500'
+        />
+
+      </form>
+
+      <div className='h-px bg-slate-400'/>
+
+      <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
+        <NewNodeCard/>
+        <NodeCard/>
+        <NodeCard/>
+        <NodeCard/>
+
+        
+
+      </div>
+
+    </div>
   )
 }
 
